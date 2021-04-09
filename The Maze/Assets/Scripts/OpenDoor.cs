d9bac69d72c173e6044a003b1cc726bbc1722d0e
berivanucan <75083740+berivanucan@users.noreply.github.com>
2021-04-09 18:39:21 +0100

    OpenDoor script is added

---
+++ The Maze/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Symbol")
+        {
+            Debug.Log("collided");
+            Destroy(gameObject);
+        }
+        
+    }
+}
