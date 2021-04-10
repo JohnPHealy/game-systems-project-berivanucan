0a14f3485e08cdb7fd598064c836588247990bde
berivanucan <75083740+berivanucan@users.noreply.github.com>
2021-04-10 04:52:15 +0100

    First Monster animations are added

---
+++ The Maze/Assets/Scripts/MonsterController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterController : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool IsWalking = animator.GetBool("IsWalking");
+        bool forwardPressed = Input.GetKey("m");
+        
+        if(!IsWalking && forwardPressed)
+        {
+            animator.SetBool("IsWalking", true);
+        }
+
+        if (IsWalking && !forwardPressed)
+        {
+            animator.SetBool("IsWalking", false);
+        }
+    }
+}
