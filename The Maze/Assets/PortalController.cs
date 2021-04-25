bac6621c592aeb9edd6b006a382723789aacbae9
berivanucan <75083740+berivanucan@users.noreply.github.com>
2021-04-25 03:50:46 +0100

    Portal model, effects, and animations are added

--- The Maze/Assets/PortalController.cs
+++ The Maze/Assets/PortalController.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class PortalController : MonoBehaviour
 {
     public GameObject instructions;
@@ -9,27 +9,57 @@ public class PortalController : MonoBehaviour
 
     public GameObject portalEffect;
     public bool isEnabled = false;
+    public bool canClose= false;
+    public float distanceFromTarget;
+    public GameObject portal;
 
     void Update()
     {
-        
+
+        distanceFromTarget =
+       Vector3.Distance(portal.transform.position, transform.position);
+
         if (Input.GetKeyDown(KeyCode.E) && isEnabled)
         {
             portalEffect.SetActive(true);
             isEnabled = false;
+            canClose = true;
             instructions.SetActive(false);
             Closeinstructions.SetActive(true);
 
         }
-        else if(Input.GetKeyDown(KeyCode.E) && !isEnabled)
+        else if(Input.GetKeyDown(KeyCode.E) && canClose)
         {
             portalEffect.SetActive(false);
             isEnabled = true;
+            canClose = false;
             instructions.SetActive(true);
             Closeinstructions.SetActive(false);
         }
+
+        if(distanceFromTarget <= 5f && !canClose)
+        {
+            isEnabled = true;
+            instructions.SetActive(true);
+            isEnabled = true;
+        }
+
+
+        if (distanceFromTarget > 5f)
+        {
+            
+            instructions.SetActive(false);
+            Closeinstructions.SetActive(false);
+        }
+
+        if (distanceFromTarget <= 2f && canClose)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
-    private void OnTriggerEnter(Collider other)
+
+
+    /*private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Portal")
         {
@@ -45,5 +75,5 @@ public class PortalController : MonoBehaviour
             instructions.SetActive(false);
             Closeinstructions.SetActive(false);
         }
-    }
+    }*/
 }
